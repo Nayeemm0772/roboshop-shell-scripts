1ed8b1572b52e9a53590b0052f7238708c01a4e9
NAYEEM <nayeem0772@gmail.com>
2022-09-22 18:55:36 +0530

    half done mysql shell script

--- mysql.sh
+++ mysql.sh
@@ -25,9 +25,36 @@ echo "Start MySQL Service"
 
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-#echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
-#FLUSH PRIVILEGES;" >/tmp/root-pass.sql
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
+FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
+echo "show databases;" |mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+  echo "Change the default root password"
+  mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
+  StatusCheck $?
+fi
+
+echo 'show plugins'| mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} 2>/dev/null | grep validate_password &>>$LOG_FILE
+if [ $? -eq 0 ]; then
+  echo "Uninstall Password Validation Plugin"
+  echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
+  StatusCheck $?
+fi
+
+echo "Download Schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
+StatusCheck $?
+
+echo "Extract Schema"
+cd /tmp
+unzip -o mysql.zip &>>$LOG_FILE
+StatusCheck $?
+
+echo "Load Schema"
+cd mysql-main
+mysql -u root -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG_FILE
+StatusCheck $?
 
 
 
