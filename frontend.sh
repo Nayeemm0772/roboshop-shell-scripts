ce28cd6bfe99c7e40ef94130274ce3270dc62fa1
NAYEEM <nayeem0772@gmail.com>
2022-09-22 17:41:01 +0530

    Edited few changes

--- frontend.sh
+++ frontend.sh
@@ -2,33 +2,32 @@ LOG_FILE=/tmp/frontend
 source common.sh
 
 echo Install Nginx
+  yum install nginx -y &>>${LOG_FILE}
+  systemctl enable nginx &>>${LOG_FILE}
+  systemctl start nginx &>>${LOG_FILE}
+  StatusCheck $?
 
-yum install nginx -y &>>${LOG_FILE}
-systemctl enable nginx &>>${LOG_FILE}
-systemctl start nginx &>>${LOG_FILE}
-
-StatusCheck $?
-
-echo download the HTDOCS content and deploy under the Nginx path
-
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
-
-StatusCheck $?
-
-echo Deploy the downloaded content in Nginx Default Location
+echo Downloading Nginx Web Content
+  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
+  StatusCheck $?
 
 cd /usr/share/nginx/html &>>${LOG_FILE}
-rm -rf * &>>${LOG_FILE}
-unzip /tmp/frontend.zip &>>${LOG_FILE}
+
+echo Removing Old Web Content
+  rm -rf * &>>${LOG_FILE}
+  StatusCheck $?
+
+echo Extracting Web Content
+  unzip /tmp/frontend.zip &>>${LOG_FILE}
+  StatusCheck $?
+
 mv frontend-main/static/* . &>>${LOG_FILE}
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
 
-StatusCheck $?
 
-echo  restart the service once to effect the changes
-systemctl restart nginx &>>${LOG_FILE}
-
-StatusCheck $?
+echo Starting Nginx Service
+  systemctl restart nginx &>>${LOG_FILE}
+  StatusCheck $?
 
 
 
