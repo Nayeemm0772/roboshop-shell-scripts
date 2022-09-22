7b2cbab283c8311b883c09d31793349c0f1587ee
NAYEEM <nayeem0772@gmail.com>
2022-09-22 19:15:51 +0530

    half done mysql shell script1

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,6 @@
 LOG_FILE=/tmp/mysql
 source common.sh
 
-if [ -z "${ROBOSHOP_MYSQL_PASSWORD}" ]; then
-  echo -e "\e[31m ROBOSHOP_MYSQL_PASSWORD env variable is needed\e[0m"
-  exit 1
-fi
-
 echo "Setting Up MySQL Repo"
   curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
   StatusCheck $?
@@ -25,58 +20,5 @@ echo "Start MySQL Service"
 
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
-FLUSH PRIVILEGES;" >/tmp/root-pass.sql
-
-echo "show databases;" |mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-  echo "Change the default root password"
-  mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
-  StatusCheck $?
-fi
-
-echo 'show plugins'| mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} 2>/dev/null | grep validate_password &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo "Uninstall Password Validation Plugin"
-  echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
-  StatusCheck $?
-fi
-
-echo "Download Schema"
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
-StatusCheck $?
-
-echo "Extract Schema"
-cd /tmp
-unzip -o mysql.zip &>>$LOG_FILE
-StatusCheck $?
-
-echo "Load Schema"
-cd mysql-main
-mysql -u root -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG_FILE
-StatusCheck $?
-
-
-
-
-# dnf module disable mysql
-
-# yum install mysql-community-server -y
-
-# systemctl enable mysqld
-# systemctl start mysqld
-
-# grep temp /var/log/mysqld.log
-
-# mysql_secure_installation
-
-# mysql -uroot -pRoboShop@1
-
-# > uninstall plugin validate_password;
-
-# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-
-# cd /tmp
-# unzip mysql.zip
-# cd mysql-main
-# mysql -u root -pRoboShop@1 <shipping.sql
+#echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
+#FLUSH PRIVILEGES;" >/tmp/root-pass.sql
