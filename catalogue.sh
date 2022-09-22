42fb40c2365de91eaa7ddf7ce62331fd2919a84b
NAYEEM <nayeem0772@gmail.com>
2022-09-22 18:04:21 +0530

    Edited few changes

--- catalogue.sh
+++ catalogue.sh
@@ -3,14 +3,13 @@ source common.sh
 
 echo  Setup NodeJS Repos
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-
   StatusCheck $?
 
 echo  Install NodeJS
   yum install nodejs -y &>>${LOG_FILE}
-
   StatusCheck $?
 
+echo adding username
 id roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
   echo Add Roboshop Application User
@@ -22,9 +21,7 @@ echo  Download Catalogue Application Code
   curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
   StatusCheck $?
 
-echo  Change the directory
-  cd /home/roboshop &>>${LOG_FILE}
-  StatusCheck $?
+cd /home/roboshop
 
 echo "Clean Old App Content"
   rm -rf catalogue &>>${LOG_FILE}
@@ -35,17 +32,18 @@ echo  extract the catalogue application code
   StatusCheck $?
 
 echo moving the catalogue-main
-
   mv catalogue-main catalogue &>>${LOG_FILE}
   StatusCheck $?
 
-echo changing the directory
-  cd /home/roboshop/catalogue &>>${LOG_FILE}
-  StatusCheck $?
+cd /home/roboshop/catalogue
+
 
 echo installing the NodeJS Dependencies
-
   npm install &>>${LOG_FILE}
+  StatusCheck $?
+
+echo Update SystemD Service File
+  sed -i -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
   StatusCheck $?
 
 echo "setup catalogue service"
